878fdea09233774b0e64348fed8e5ea49c80ee2b
kimkoo23 <sedreon@gmail.com>
2021-12-22 20:10:11 +0900

    id변경,pw변경 화면 적용버젼

---
+++ src/main/java/com/project/member/controller/IdController.java
@@ -0,0 +1,15 @@
+package com.project.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user-find")
+public class IdController {
+
+    @GetMapping
+    public String modify(){
+        return "modify/user-find";
+    }
+}
---
+++ src/main/java/com/project/member/controller/PwController.java
@@ -0,0 +1,15 @@
+package com.project.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pass-find")
+public class PwController {
+
+    @GetMapping
+    public String modify() {
+        return "modify/pass-find";
+    }
+}
