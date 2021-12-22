ed358f0732e7f89c898ae9b07c867bcc893cedf2
su <yyy1247@gmail.com>
2021-12-22 18:17:35 +0900

    오류 수정

--- src/main/java/com/project/MovieApplication.java
+++ src/main/java/com/project/MovieApplication.java
@@ -1,9 +1,11 @@
-package com.project.movie;
+package com.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class MovieApplication {
 
 	public static void main(String[] args) {
