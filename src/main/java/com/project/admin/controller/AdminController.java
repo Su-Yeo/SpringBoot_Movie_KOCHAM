f032bc4aacc25d5026cc6b72a96346c62fe13935
skycar321 <skycar321@naver.com>
2021-12-23 17:35:40 +0900

    12/23 17:53 관리자페이지 틀 생성 완료 http://localhost:8080/admin/main
    관리자페이지 틀안에서 각자 관리자 페이지 생성하시면 됩니다

---
+++ src/main/java/com/project/admin/controller/AdminController.java
@@ -0,0 +1,45 @@
+package com.project.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/admin")
+public class AdminController {
+
+    @GetMapping(value = "/main") //관리자 메인 페이지
+    public String adminMain( Model model){
+
+        return "admin/adminMain";
+    }
+
+    @GetMapping(value = "/schedule") //상영스케줄 관리
+    public String adminSchedule( Model model){
+
+        return "admin/adminSchedule";
+    }
+
+
+    @GetMapping(value = "/member") //회원
+    public String adminMember( Model model){
+
+        return "admin/adminMember";
+    }
+
+    @GetMapping(value = "/movie") //영화
+    public String adminMovie( Model model){
+
+        return "admin/adminMovie";
+    }
+
+    @GetMapping(value = "/event") //이벤트
+    public String adminEvent( Model model){
+
+        return "admin/adminEvent";
+    }
+
+
+
+}
