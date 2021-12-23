a4a3f6e9c18515671f377b53d123b681171e7615
sumin5 <osn9274@naver.com>
2021-12-23 17:56:24 +0900

    test용

---
+++ src/main/java/com/project/member/dto/BookingDto.java
@@ -0,0 +1,9 @@
+package com.project.member.dto;
+
+public class BookingDto {
+
+    private int movieNum;
+    private String infoCity;
+    private String infoDist;
+    private String startTime;
+}
--- src/main/java/com/project/reservation/controller/ReservationController.java
+++ src/main/java/com/project/reservation/controller/ReservationController.java
@@ -1,13 +1,45 @@
 package com.project.reservation.controller;
 
+import com.project.member.dto.BookingDto;
+import groovy.util.logging.Log4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+
+@Log4j
 @Controller
-@RequestMapping(value="/reservation")
+@RequestMapping(value="/movie/screening")
 public class ReservationController {
+
+//    @GetMapping("/booking")
+//    public String doBooking(@RequestParam(value = "movie_num", required = false) int movie_num, Model model)
+//            throws Exception {
+//
+//        System.out.println("##################################movie_num:"+movie_num);
+//        BookingDto dto = new BookingDto();
+//        model.addAttribute("movieRead", s2.movieRead(movie_num));
+//
+//        System.out.println("##################################movie_num:"+s2.movieRead(movie_num));
+//
+//        // 영화정보 페이지에서 예약버튼을 누르면 해당 영화번호를 받아와서 set으로 설정한다.
+//        dto.setMovieNum(movie_num);
+//
+//        List<String> list = service.getCities(dto);
+//        model.addAttribute("list", list); // 여기까지 했고 jsp에 for-each문 생각해보자
+//
+//        return "reserve";
+//    }// doBooking
+
+
+
+
+
+
+
+
 
     @GetMapping(value = "/choiceMovie")
     public String reservationMovie( Model model){
