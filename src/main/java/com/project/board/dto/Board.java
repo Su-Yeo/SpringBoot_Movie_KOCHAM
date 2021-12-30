7d8702b0ace645cd3962f7b0bdf591db2be796ef
Hanheejoo <gksgmlwn99111@naver.com>
2021-12-30 16:32:51 +0900

    이벤트 페이지 수정, 삭제 기능 추가

--- src/main/java/com/project/board/dto/Board.java
+++ src/main/java/com/project/board/dto/Board.java
@@ -1,6 +1,8 @@
 package com.project.board.dto;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Entity;
@@ -10,6 +12,8 @@ import javax.persistence.Id;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 @Entity
 @Data
 public class Board {
@@ -23,4 +27,12 @@ public class Board {
     private LocalDate board_end_date; //이벤트 종료날짜
     private String board_image; //이벤트 내용 이미지url
     private String board_thumbnail; //이벤트 썸네일 이미지 url
+
+    public void updateBoard(String board_title, LocalDate board_start_date, LocalDate board_end_date, String board_image, String board_thumbnail) {
+        this.board_title = board_title;
+        this.board_start_date = board_start_date;
+        this.board_end_date = board_end_date;
+        this.board_image = board_image;
+        this.board_thumbnail = board_thumbnail;
+    }
 }
