309557bc9d6c441db8a892e57cdc91eeeac682bb
su <yyy1247@gmail.com>
2021-12-30 18:09:56 +0900

    결제  페이지 기능 수정 (미완성)

--- src/main/java/com/project/order/dto/GifticonDTO.java
+++ src/main/java/com/project/order/dto/GifticonDTO.java
@@ -15,12 +15,20 @@ import java.time.LocalDateTime;
 public class GifticonDTO { //기프티콘 테이블
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
-    private Long gift_num; //키프티콘코드
+    @Column(nullable = false, name="gift_num")
+    private Long giftNum; //기프티콘코드
+    @Column(nullable = false)
     private String member_id; //아이디(구매자)
+    @Column(nullable = false)
     private Long product_num; //상품코드
+    @Column(nullable = false)
     private String product_name; //상품명
+    @Column(nullable = false)
     private String order_from_id; //보내신분
+    @Column(nullable = false)
     private LocalDateTime gift_exp; //유효기간
+    @Column(nullable = false)
     private LocalDateTime order_date; //받은 날짜
-
+    @Column(nullable = false)
+    private int product_price; //기프티콘금액
 }
