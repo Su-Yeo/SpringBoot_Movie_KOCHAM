3ad682749af43ef626618eb32f6b317d3a1c9b55
skycar321 <skycar321@naver.com>
2021-12-30 17:13:10 +0900

    예약쪽 수정중
    12.30 17:12
    남진황

--- src/main/java/com/project/reservation/movie/dto/ReservationMovieDTO.java
+++ src/main/java/com/project/reservation/movie/dto/ReservationMovieDTO.java
@@ -16,43 +16,60 @@ import java.sql.Date;
 @NoArgsConstructor
 
 public class ReservationMovieDTO {
-    private Long schedule_num;
-    private int movie_num =0;
-    private int theater_area_num =0;
 
-    private int schedule_start=0;
-    private int schedule_end=0;
-    private int schedule_cost =9000;
 
+    private Long ticket_num ;
+    private int seat_num ;
+    private int ticket_group ;
+    private int movie_num ;
+    private int schedule_num ;
+    private int theater_area_num ;
+    private String member_id ;
+    private Date ticket_date ;
+    private String theater_name;
+    private String theater_city;
+    private String theater_loc;
+    private int theater_totalSeat;
+    private int schedule_start;
+    private int schedule_end;
     private Date schedule_date ;
-
-    private String file_name="";
-    private long file_size=1;
 
 
 
     public ReservationMovie toEntity( ) {
         return ReservationMovie.builder()
-            .schedule_date(schedule_date)
+            .ticket_num(ticket_num)
+            .seat_num(seat_num)
             .movie_num(movie_num)
+            .schedule_num(schedule_num)
             .theater_area_num(theater_area_num)
+            .member_id(member_id)
+            .ticket_date(ticket_date)
+            .theater_name(theater_name)
+            .theater_city(theater_city)
+            .theater_loc(theater_loc)
+            .theater_totalSeat(theater_totalSeat)
             .schedule_start(schedule_start)
             .schedule_end(schedule_end)
-            .schedule_cost(schedule_cost)
-            .file_name(file_name)
-            .file_size(file_size)
+            .schedule_date(schedule_date)
+
             .build();
     }
-    public ReservationMovieDTO(ReservationMovie schedule) {
-        this.schedule_num=schedule.getSchedule_num();
-        this.movie_num = schedule.getMovie_num();
-        this.theater_area_num = schedule.getTheater_area_num();
-        this.schedule_start = schedule.getSchedule_start();
-        this.schedule_end = schedule.getSchedule_end();
-        this.schedule_cost = schedule.getSchedule_cost();
-        this.file_name = schedule.getFile_name();
-        this.file_size = schedule.getFile_size();
-        this.schedule_date = schedule.getSchedule_date();
+    public ReservationMovieDTO(ReservationMovie RM) {
+        this.ticket_num=RM.getTicket_num();
+        this.seat_num = RM.getSeat_num();
+        this.movie_num = RM.getMovie_num();
+        this.schedule_num = RM.getSchedule_num();
+        this.theater_area_num = RM.getTheater_area_num();
+        this.member_id = RM.getMember_id();
+        this.ticket_date = RM.getTicket_date();
+        this.theater_name = RM.getTheater_name();
+        this.theater_city = RM.getTheater_city();
+        this.theater_loc = RM.getTheater_loc();
+        this.theater_totalSeat = RM.getTheater_totalSeat();
+        this.schedule_start = RM.getSchedule_start();
+        this.schedule_end = RM.getSchedule_end();
+        this.schedule_date = RM.getSchedule_date();
 
     }
 
